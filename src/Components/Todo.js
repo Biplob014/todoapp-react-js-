61b6bb36e2a76280098abde78665f78293472909
Biplob <biplobcs514@gmail.com>
2022-08-08 11:10:56 +0600

    updated todo.js

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -3,32 +3,6 @@ import { FaTrashAlt } from "react-icons/fa";
 // imported file
 import style from "./todo.module.css";
 
-// const Todo = (props) => {
-//   const { id, title, desc } = props.todo;
-
-//   // passing deleted id to the todos
-//   const handleClick = (id) => props.onRemoveItem(id);
-
-//   return (
-//     <article className={style.todo}>
-//       <div>
-//         <h3>{title}</h3>
-//         <p>{desc}</p>
-//       </div>
-
-//       <div>
-//         <button className={style.btn} onClick={() => handleClick(id)}>
-//           <i className="fa fa-trash fa-2x"></i>
-//         </button>
-//       </div>
-//     </article>
-//   );
-// };
-// export default Todo;
-
-/////////////////////////////////
-// practice
-// /////////////////////////
 const Todo = (props) => {
   const { id, title, desc } = props.todo;
 

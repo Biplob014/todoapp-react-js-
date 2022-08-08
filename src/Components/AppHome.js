6d20de6edaea5209e3ec1352b1a74317f43c7f6f
Biplob <biplobcs514@gmail.com>
2022-08-08 11:08:26 +0600

    updated apphome

--- src/Components/AppHome.js
+++ src/Components/AppHome.js
@@ -6,66 +6,6 @@ import NewToDo from "./NewToDo";
 import style from "./apphome.module.css";
 import ShowMessage from "./ShowMessage";
 
-// const AppHome = () => {
-//   const [todos, setTodos] = useState([]);
-//   const [isModalOpen, setIsmodalOpen] = useState(false);
-//   const [modalText, setModalText] = useState("");
-
-//   const handleOntodo = (container) => {
-//     setTodos((previousTodos) => {
-//       return [...previousTodos, { id: uuidv4(), ...container }];
-//     });
-
-//     // showing message for adding todo
-//     setIsmodalOpen(true);
-//     return (
-//       setTimeout(() => {
-//         setIsmodalOpen(false);
-//       }, 1000),
-//       setModalText("Todo is added")
-//     );
-//   };
-
-//   // deleting item
-//   const handleonRemoveItem = (id) => {
-//     // const filterTodos = todos.filter((todo) => todo.id !== id);
-//     // setTodos(filterTodos);
-
-//     // anothear way
-//     setTodos((previousTodos) => {
-//       const filterTodos = previousTodos.filter((todo) => todo.id !== id);
-//       return filterTodos;
-//     });
-
-//     // showing message for removing todo
-//     setIsmodalOpen(true);
-//     return (
-//       setTimeout(() => {
-//         setIsmodalOpen(false);
-//       }, 1000),
-//       setModalText("Todo is removed")
-//     );
-//   };
-
-//   return (
-//     <div className={style.container}>
-//       <h1 style={{ color: "white" }}>Todo App</h1>
-//       <NewToDo onTodo={handleOntodo} />
-
-//       <Todos
-//         todos={todos}
-//         addmsg={isModalOpen && modalText}
-//         onRemoveItem={handleonRemoveItem}
-//       />
-//     </div>
-//   );
-// };
-
-// export default AppHome;
-
-// //////////////////////////////////
-// practice
-// ////////////////////////////////
 const AppHome = () => {
   const [todos, setTodos] = useState([]);
   const [isModalOpen, setIsmodalOpen] = useState(false);

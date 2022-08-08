1582ee77b2871f5c2cec585999e33359a25388ce
Biplob <biplobcs514@gmail.com>
2022-08-08 11:11:35 +0600

    update todos.js

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -4,25 +4,6 @@ import Todo from "./Todo";
 import style from "./todos.module.css";
 import ShowMessage from "./ShowMessage";
 
-// const Todos = (props) => {
-//   // passing deleted object id to the Arary/home
-//   const handleonRemoveItem = (id) => props.onRemoveItem(id);
-
-//   return (
-//     <section className={style.todos}>
-//       <ShowMessage modalText={props.addmsg} />
-//       {props.todos.map((todo) => (
-//         <Todo todo={todo} key={todo.id} onRemoveItem={handleonRemoveItem} />
-//       ))}
-//     </section>
-//   );
-// };
-
-// export default Todos;
-
-// ////////////////////////////////
-// practice
-////////////////////////////////
 const Todos = (props) => {
   const handleRemoveId = (id) => {
     props.removeId(id);

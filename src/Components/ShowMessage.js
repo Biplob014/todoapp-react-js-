6595e014f8e52c5a863ec1a9eee117020818ecb3
Biplob <biplobcs514@gmail.com>
2022-08-08 11:10:21 +0600

    updated showmessage

--- src/Components/ShowMessage.js
+++ src/Components/ShowMessage.js
@@ -2,18 +2,6 @@ import React from "react";
 // imported file
 import style from "./showmessage.module.css";
 
-// const ShowMessage = (props) => {
-//   return (
-//     <div className={style["msg-container"]}>
-//       <p className={style["msg-p"]}>{props.modalText}</p>
-//     </div>
-//   );
-// };
-// export default ShowMessage;
-
-// ////////////////////////////////
-// practice
-// /////////////////////////////
 const ShowMessage = (props) => {
   return (
     <div className={style["msg-container"]}>
